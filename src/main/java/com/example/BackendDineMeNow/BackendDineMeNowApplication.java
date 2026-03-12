1b3d8f12ebd6c7e469f421160577646f0105e88c
Jeimy paez <jeimypaez@gmail.com>
2026-03-11 19:26:35 -0500

    inicializacion

---
+++ src/main/java/com/example/BackendDineMeNow/BackendDineMeNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendDineMeNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDineMeNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDineMeNowApplication.class, args);
+	}
+
+}
